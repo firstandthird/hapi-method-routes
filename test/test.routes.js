4cf2320ca6a90931006dfb76cd0557dde1723a08
Chris <orthagonal1@gmail.com>
2017-04-24 16:42:29 -0500

    example -> npm tests

---
+++ test/test.routes.js
@@ -0,0 +1,92 @@
+'use strict';
+const tap = require('tap');
+const Hapi = require('hapi');
+const routeModule = require('../index.js');
+
+let server;
+
+tap.beforeEach((done) => {
+  server = new Hapi.Server({
+    debug: {
+      log: ['info', 'error']
+    },
+  });
+  server.connection({ port: 3000 });
+  server.register(
+    {
+      register: routeModule,
+      options: {}
+    }, () => {
+    server.start(done);
+  });
+});
+tap.afterEach((done) => {
+  server.stop(done);
+});
+
+tap.test('lets you register a method with the server and call it as a route', (t) => {
+  server.method('add', (a, b, done) => done(null, a + b), {});
+  server.inject({
+    url: '/methods/add',
+    method: 'POST',
+    payload: {
+      values: [20, 25]
+    }
+  }, response => {
+    t.equal(response.statusCode, 200);
+    t.equal(response.result.successful, true);
+    t.equal(response.result.result, '2025');
+    t.end();
+  });
+});
+
+tap.test('lets you register a sub-document method with the server and call it as a route', (t) => {
+  server.method('math.add', (a, b, done) => {
+    try {
+      const floatA = parseFloat(a);
+      const floatB = parseFloat(b);
+      return done(null, floatA + floatB);
+    } catch (exc) {
+      return done(exc);
+    }
+  }, {});
+  server.inject({
+    url: '/methods/math.add',
+    method: 'POST',
+    payload: {
+      values: [20, 25]
+    }
+  }, response => {
+    t.equal(response.statusCode, 200);
+    t.equal(response.result.successful, true);
+    t.equal(response.result.result, 45);
+    t.end();
+  });
+});
+
+tap.test('returns error when wrong number of parameters is passed', (t) => {
+  server.method('add', (a, b, done) => done(null, a + b), {});
+  server.inject({
+    url: '/methods/add',
+    method: 'POST',
+    payload: {
+    }
+  }, response => {
+    t.equal(response.statusCode, 404);
+    t.equal(response.result, 'Method name add failed. Error: TypeError: done is not a function');
+    t.end();
+  });
+});
+
+tap.test('return 404 when you call a method that does not exist', (t) => {
+  server.inject({
+    url: '/methods/math.add',
+    method: 'POST',
+    payload: {
+      values: [20, 25]
+    }
+  }, response => {
+    t.equal(response.statusCode, 404);
+    t.end();
+  });
+});
