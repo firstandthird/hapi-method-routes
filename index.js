5ebf67771b3966c30548262508153aa7bd1c0703
Chris <orthagonal1@gmail.com>
2017-04-24 19:10:08 -0500

    only use payload.method to call methods

--- index.js
+++ index.js
@@ -4,47 +4,29 @@ const str2fn = require('str2fn');
 const defaults = {
   // option to change the endpoint:
   endpoint: '/methods',
-  auth: null,
-  // optional whitelist function to screen requests to a method:
-  whitelist(methodName, request) {
-    // by default we always allow access:
-    return true;
-  }
+  auth: null
 };
 
 exports.register = (server, options, next) => {
   // setup defaults:
   const settings = Object.assign({}, defaults, options);
   const endpoint = settings.endpoint;
-  const whitelist = settings.whitelist;
   server.route({
     method: 'POST',
-    path: `${endpoint}/{methodName}/{params*}`,
+    path: `${endpoint}`,
     config: {
       auth: settings.auth
     },
     handler(request, reply) {
-      // determine the name of the method they are trying to call:
-      const methodName = typeof request.payload.method === 'string' ? request.payload.method : decodeURIComponent(request.params.methodName);
-      // stop them if they don't have access to the indicated method:
-      if (!whitelist(methodName, request)) {
-        return reply({ successful: false, result: `You do not have access to ${methodName}` }).code(550);
-      }
-      const method = str2fn(server.methods, methodName, () => reply({ successful: false, result: `Method name ${methodName} does not exist ` }).code(404));
-      const params = request.payload.values || request.params.params || [];
-      params.push((err, result) => {
+      str2fn.execute(request.payload.method, server.methods, {}, (err, result) => {
         if (err !== null) {
-          reply({ successful: false, result: `Method name ${methodName} threw this error: ${err}` }).code(500);
-        } else {
-          reply({ successful: true, result });
+          if (err.toString().indexOf('does not exist') > -1) {
+            return reply({ successful: false, result: `Method call ${request.payload.method} invokes a method that is not defined` }).code(404);
+          }
+          return reply({ successful: false, result: `Method call ${request.payload.method} threw this error: ${err}` }).code(500);
         }
+        return reply({ successful: true, result });
       });
-      try {
-        method.apply(null, params);
-      } catch (exc) {
-        server.log(['hapi-method-routes', 'error'], exc);
-        return reply(`Method name ${methodName} failed. Error: ${exc}`).code(404);
-      }
     }
   });
   next();
--- test/test.routes.js
+++ test/test.routes.js
@@ -27,10 +27,10 @@ tap.afterEach((done) => {
 tap.test('lets you register a method with the server and call it as a route', (t) => {
   server.method('add', (a, b, done) => done(null, a.toString() + b.toString()), {});
   server.inject({
-    url: '/methods/add',
+    url: '/methods',
     method: 'POST',
     payload: {
-      values: [20, 25]
+      method: 'add(20, 25)'
     }
   }, response => {
     t.equal(response.statusCode, 200);
@@ -40,7 +40,7 @@ tap.test('lets you register a method with the server and call it as a route', (t
   });
 });
 
-tap.test('lets you register a sub-document method with the server and call it as a route', (t) => {
+tap.test('lets you expose a sub-document method with the server and call it as a route', (t) => {
   server.method('math.add', (a, b, done) => {
     try {
       const floatA = parseFloat(a);
@@ -51,10 +51,10 @@ tap.test('lets you register a sub-document method with the server and call it as
     }
   }, {});
   server.inject({
-    url: '/methods/math.add',
+    url: '/methods',
     method: 'POST',
     payload: {
-      values: [20, 25]
+      method: 'math.add(20, 25)'
     }
   }, response => {
     t.equal(response.statusCode, 200);
@@ -63,46 +63,31 @@ tap.test('lets you register a sub-document method with the server and call it as
     t.end();
   });
 });
-
-tap.test('returns 404 when wrong number of parameters is passed', (t) => {
+tap.test('returns 500 when wrong number of parameters is passed', (t) => {
   server.method('add', (a, b, done) => done(null, a + b), {});
   server.inject({
-    url: '/methods/add',
+    url: '/methods',
     method: 'POST',
     payload: {
+      method: 'add()'
     }
   }, response => {
-    t.equal(response.statusCode, 404);
+    t.equal(response.statusCode, 500);
     t.end();
   });
 });
 
 tap.test('return 404 when you call a method that does not exist', (t) => {
   server.inject({
-    url: '/methods/math.add',
+    url: '/methods',
     method: 'POST',
     payload: {
-      values: [20, 25]
+      method: 'add(20, 25)'
     }
   }, response => {
     t.equal(response.statusCode, 404);
-    t.end();
-  });
-});
-
-tap.test('lets you specify the method to call inside a POST payload', (t) => {
-  server.method('add', (a, b, done) => done(null, a.toString() + b.toString()), {});
-  server.inject({
-    url: '/methods/',
-    method: 'POST',
-    payload: {
-      method: 'add',
-      values: [20, 25]
-    }
-  }, response => {
-    t.equal(response.statusCode, 200);
-    t.equal(response.result.successful, true);
-    t.equal(response.result.result, '2025');
+    t.equal(response.result.successful, false);
+    t.equal(response.result.result, 'Method call add(20, 25) invokes a method that is not defined');
     t.end();
   });
 });
